0c8893a9bbfdb23636740b21a9a3b9f8e3b828f9
imronha <imronha@gmail.com>
2023-11-27 13:23:26 -0800

    connect pokemon api and get names

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pokemons: [],
+      searchField: ''
+    }
+  }
+  
+  componentDidMount(){
+    // console.log('componentDidMount')
+    fetch('https://pokeapi.co/api/v2/pokemon/')
+      .then(response => response.json())
+      .then(pokemon => 
+        this.setState(
+          () => {
+            console.log(pokemon)
+            return {pokemons: pokemon.results};
+          },
+          // () => {
+          //   console.log(this.state);
+          // }
+        )
+      )
+  }
+  
+  // onSearchChange = (event) => {
+  //   // console.log(event.target.value);
+  //   const searchField = event.target.value.toLocaleLowerCase();
+  //   this.setState(() => {
+  //     return { searchField }
+  //   })
+  // }
+
+  render() {
+    // console.log('render');
+
+    const { pokemons, searchField} = this.state;
+    // const { onSearchChange } = this;
+
+    // const filteredPokemon = pokemons.filter((pokemon) => {
+    //   return pokemon.name.toLocaleLowerCase().includes(searchField)
+    // });
+
+    return (
+      <div className="App">
+        <h1 className='app-title'>Pokedex</h1>
+        <div className='poke-card'>
+          {pokemons.map((pokemon) => {
+            return (
+              <h2>{pokemon.name}</h2>
+            )
+          })}
+        </div>
+        {console.log(pokemons)}
+      </div>
+    );
+  }
 }
 
 export default App;
