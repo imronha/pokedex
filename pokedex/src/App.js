0733ab7dcc26e1e17b7c265ae091b1b58d0e8453
imronha <imronha@gmail.com>
2023-11-28 08:55:17 -0800

    search box created

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import SearchBox from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -10,7 +11,6 @@ class App extends Component {
   }
   
   componentDidMount(){
-    // console.log('componentDidMount')
     fetch('https://pokeapi.co/api/v2/pokemon/')
       .then(response => response.json())
       .then(pokemon => 
@@ -19,26 +19,24 @@ class App extends Component {
             console.log(pokemon)
             return {pokemons: pokemon.results};
           },
-          // () => {
-          //   console.log(this.state);
-          // }
         )
       )
   }
   
-  // onSearchChange = (event) => {
-  //   // console.log(event.target.value);
-  //   const searchField = event.target.value.toLocaleLowerCase();
-  //   this.setState(() => {
-  //     return { searchField }
-  //   })
-  // }
+  onSearchChange = (event) => {
+    // console.log(event.target.value);
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField }
+    })
+  }
 
   render() {
     // console.log('render');
 
     const { pokemons, searchField} = this.state;
-    // const { onSearchChange } = this;
+    console.log(pokemons + 'in app.js')
+    const { onSearchChange } = this;
 
     // const filteredPokemon = pokemons.filter((pokemon) => {
     //   return pokemon.name.toLocaleLowerCase().includes(searchField)
@@ -47,18 +45,22 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className='app-title'>Pokedex</h1>
+        <SearchBox 
+          className = 'pokemon-search-box'
+          onChangeHandler = { onSearchChange } 
+          placeHolder = 'search pokemon'>
+        </SearchBox>
         <div className='poke-card'>
-          {pokemons.map((pokemon, index) => {
+            {pokemons.map((pokemon, index) => {
             return (
-              <div>
+                <div>
                 <h2>#{index+1} {pokemon.name.charAt(0).toUpperCase()+pokemon.name.slice(1)} </h2>
                 <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index+1}.svg`} width="100" height="100"></img>
-              </div>
+                </div>
 
             )
-          })}
+            })}
         </div>
-        {console.log(pokemons)}
       </div>
     );
   }
